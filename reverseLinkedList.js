bd569965341c9f148daa3aaa6a3969f5e7a266c1
Sujwal <dsujwal24@gmail.com>
2026-01-17 12:21:44 +0545

    check isPalindrome

--- reverseLinkedList.js
+++ reverseLinkedList.js
@@ -3,48 +3,53 @@ function Node(value, next = null) {
   this.next = next;
 }
 
-let first = new Node(2);
-let second = new Node(30);
-let third = new Node(50);
-let fourth = new Node(70);
-let fifth = new Node(80);
-let sixth = new Node(90);
+let first = new Node(1);
+let second = new Node(2);
+let third = new Node(3);
+let fourth = new Node(4);
+let fifth = new Node(3);
+let sixth = new Node(2);
+let seventh = new Node(1);
 
 first.next = second;
-second.next = third;
-third.next = fourth;
-fourth.next = fifth;
-fifth.next = sixth;
+// second.next = third;
+// third.next = fourth;
+// fourth.next = fifth;
+// fifth.next = sixth;
+// sixth.next = seventh;
 let head = first;
 
 // Reversal using pointer
 //
-// function reversal(head) {
+// function reverse(head) {
 //   let prev = null;
 //   let curr = head;
 
 //   while (curr !== null) {
-//     let next = curr.next;
-//     curr.next = prev;
-//     prev = curr;
+//     let next = curr.next; //save
+//     curr.next = prev; // reverse
+//     prev = curr; //
 //     curr = next;
 //   }
 //   return prev;
 // }
+// console.log(reverse(head));
+// 2, 30, 50, 70
+// function reversalRecursion(head) {
+//   if (head == null || head.next == null) return head;
 
-// console.log(reversal(head));
+//   let newHead = reversalRecursion(head.next);
 
-//
-function recursiveReversal(head) {
-  if (head == null || head.next == null) return head;
-  let newHead = recursiveReversal(head.next);
-  head.next.next = head;
-  head.next = null;
-  return newHead;
-}
+//   let front = head.next;
+//   front.next = head;
+//   head.next = null;
 
-console.log(recursiveReversal(head));
-//
+//   return newHead;
+// }
+
+// console.log(reversalRecursion(head));
+
+// //
 
 //
 
@@ -68,3 +73,62 @@ console.log(recursiveReversal(head));
 // console.log(rev(head));
 
 // Reversal using pointer
+
+// head = 1, 2, 2, 1
+
+// bruteforce palindrome
+// var isPalindrome = function (head) {
+//   let temp = head;
+//   let stack = [];
+
+//   while (temp !== null) {
+//     // console.log(temp);
+//     stack.push(temp.value);
+//     temp = temp.next;
+//   }
+//   temp = head;
+//   for (let i = stack.length - 1; i >= 0; i--) {
+//     if (stack[i] != temp.value) return false;
+//     temp = temp.next;
+//   }
+//   return true;
+// };
+
+// optimal palindrome
+
+function reversalRecursion(head) {
+  if (head == null || head.next == null) return head;
+  let newHead = reversalRecursion(head.next);
+  let front = head.next;
+  front.next = head;
+  head.next = null;
+
+  return newHead;
+}
+
+var isPalindrome = function (head) {
+  let slow = head;
+  let fast = head;
+
+  while (fast.next != null && fast.next.next != null) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  let newHead = reversalRecursion(slow.next);
+
+  let first = head;
+  let second = newHead;
+
+  while (second != null) {
+    if (first.value != second.value) {
+      reversalRecursion(newHead);
+      return false;
+    }
+    first = first.next;
+    second = second.next;
+  }
+  reversalRecursion(newHead);
+  return true;
+};
+// 33, 44, 55, 66, 77, 88, 99
+console.log(isPalindrome(head));
