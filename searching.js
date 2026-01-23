1cbda6f0d069bf39880497d8a09304323c668ae9
Sujwal <dsujwal24@gmail.com>
2026-01-23 10:43:34 +0545

    Searching problems

--- searching.js
+++ searching.js
@@ -1,5 +1,5 @@
 // let nums = [1, 2, 3, 4, 5, 6, 6, 6, 6, 6, 7, 8];
-let nums = [5, 7, 7, 8, 8, 10];
+// let nums = [5, 7, 7, 8, 8, 10];
 
 // function binarySearch(nums, target) {
 //   let left = 0;
@@ -79,49 +79,69 @@ let nums = [5, 7, 7, 8, 8, 10];
 //   return ans;
 // }
 
-var searchRange = function (nums, target) {
-  function firstOccurance(nums, target) {
-    let first = -1;
-    let low = 0;
-    let high = nums.length - 1;
+// var searchRange = function (nums, target) {
+//   function firstOccurance(nums, target) {
+//     let first = -1;
+//     let low = 0;
+//     let high = nums.length - 1;
 
-    while (low <= high) {
-      let mid = Math.floor((low + high) / 2);
-      if (nums[mid] === target) {
-        first = mid;
-        high = mid - 1;
-      } else if (nums[mid] < target) {
-        low = mid + 1;
-      } else {
-        high = mid - 1;
-      }
+//     while (low <= high) {
+//       let mid = Math.floor((low + high) / 2);
+//       if (nums[mid] === target) {
+//         first = mid;
+//         high = mid - 1;
+//       } else if (nums[mid] < target) {
+//         low = mid + 1;
+//       } else {
+//         high = mid - 1;
+//       }
+//     }
+//     return first;
+//   }
+
+//   function lastOccurance(nums, target) {
+//     let last = -1;
+//     let low = 0;
+//     let high = nums.length - 1;
+
+//     while (low <= high) {
+//       let mid = Math.floor((low + high) / 2);
+//       if (nums[mid] == target) {
+//         last = mid;
+//         low = mid + 1;
+//       } else if (nums[mid] < target) {
+//         low = mid + 1;
+//       } else {
+//         high = mid - 1;
+//       }
+//     }
+//     return last;
+//   }
+
+//   let first = firstOccurance(nums, target);
+//   if (first == -1) return [-1, -1];
+//   let last = lastOccurance(nums, target);
+//   return [first, last];
+// };
+
+let nums = [3, 4, 5, 6];
+
+var searchInsert = function (nums, target) {
+  let low = 0;
+  let high = nums.length;
+  let ans = nums.length;
+
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+
+    if (nums[mid] >= target) {
+      ans = mid;
+      high = mid - 1;
+    } else {
+      low = mid + 1;
     }
-    return first;
   }
-
-  function lastOccurance(nums, target) {
-    let last = -1;
-    let low = 0;
-    let high = nums.length - 1;
-
-    while (low <= high) {
-      let mid = Math.floor((low + high) / 2);
-      if (nums[mid] == target) {
-        last = mid;
-        low = mid + 1;
-      } else if (nums[mid] < target) {
-        low = mid + 1;
-      } else {
-        high = mid - 1;
-      }
-    }
-    return last;
-  }
-
-  let first = firstOccurance(nums, target);
-  if (first == -1) return [-1, -1];
-  let last = lastOccurance(nums, target);
-  return [first, last];
+  return ans;
 };
 
-console.log(searchRange(nums, 8));
+console.log(searchInsert(5, 6));
---
+++ sorting.js
@@ -0,0 +1,13 @@
+let arr = [1, 5, 2, 3, 7, 4, 6];
+
+function bubbleSort(arr) {
+  let n = arr.length;
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+  return arr;
+}
