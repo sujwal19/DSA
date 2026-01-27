4d4eb652a41ae924ea78b03c5a86f7b963daeb3c
Sujwal <dsujwal24@gmail.com>
2026-01-27 21:50:42 +0545

    Valid Parentheses in progress

--- stacksandqueues.js
+++ stacksandqueues.js
@@ -97,7 +97,90 @@
 //
 
 // Valid Parentheses
-let s = "()[]{}";
-var isValid = function (s) {};
+let s = "([])";
 
-console.log(isValid(s));
+var isValid1 = function (s) {
+  let stack = [];
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] == "(") stack.push(")");
+    else if (s[i] == "{") stack.push("}");
+    else if (s[i] == "[") stack.push("]");
+    else if (s[i] == ")" || s[i] == "}" || s[i] == "]") {
+      if (stack.length == 0) return false;
+      else if (s[i] != stack[stack.length - 1]) return false;
+      else stack.pop();
+    }
+  }
+  return stack.length == 0;
+};
+
+// var isValid2 = function (s) {
+//   let stack = [];
+//   for (let i = 0; i < s.length; i++) {
+//     if (s[i] == "(" || s[i] == "{" || s[i] == "[") {
+//       stack.push(s[i]);
+//     } else {
+//       if (stack[stack.length - 1] != s[i]) {
+//         stack.pop();
+//       }
+//     }
+//   }
+//   return stack.length == 0;
+// };
+
+console.log(isValid1(s));
+// console.log(isValid2(s));
+
+// var isValid = function (s) {
+//   const stack = [];
+//   const dic = {
+//     ")": "(",
+//     "}": "{",
+//     "]": "[",
+//   };
+
+//   for (let c of s) {
+//     if (!dic[c]) {
+//       // opening
+//       stack.push(c);
+//     } else {
+//       if (stack[stack.length - 1] === dic[c]) {
+//         stack.pop();
+//       } else {
+//         return false;
+//       }
+//     }
+//   }
+//   return !stack.length;
+// };
+
+// var isValid = function (s) {
+//   // base case
+//   if (s.length <= 1 || s[0] == "}" || s[0] == "]" || s[0] == ")") {
+//     return false;
+//   }
+
+//   let validClose = {
+//     "[": "]",
+//     "{": "}",
+//     "(": ")",
+//   };
+//   var outputArr = []; // this array will have only open brackets lie { ( [
+//   outputArr.push(s[0]); // default push the first item to output array
+//   for (let index = 1; index < s.length; index++) {
+//     const element = s[index];
+//     // if the element is a closing item and it is valid closing element.
+//     // we will not add it to the output array instead we will remove last item of the array
+//     if (s[index] == "}" || s[index] == "]" || s[index] == ")") {
+//       if (s[index] == validClose[outputArr[outputArr.length - 1]]) {
+//         outputArr.pop();
+//       } else {
+//         return false;
+//       }
+//     } else {
+//       outputArr.push(s[index]);
+//     }
+//   }
+//   return outputArr.length > 0 ? false : true;
+// };
+// see valid patentheses solutions ------------------------------ IMP --------------------------------------------------
