8c79aeaf52e83a64636a2c3d976f8cf236600670
Sujwal <dsujwal24@gmail.com>
2026-02-04 20:49:08 +0545

    Container with most water

--- twoPointers.js
+++ twoPointers.js
@@ -160,4 +160,29 @@ var minimumLength = function (s) {
   }
   return right - left + 1;
 };
-console.log(minimumLength(str1));
+// console.log(minimumLength(str1));
+
+//
+
+//
+let height = [1, 8, 6, 2, 5, 4, 8, 3, 7];
+
+var maxArea = function (height) {
+  let left = 0;
+  let right = height.length - 1;
+  let maxWater = 0;
+
+  while (left < right) {
+    let area = (right - left) * Math.min(height[left], height[right]);
+
+    maxWater = Math.max(maxWater, area);
+
+    if (height[left] < height[right]) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+  return maxWater;
+};
+console.log(maxArea(height));
