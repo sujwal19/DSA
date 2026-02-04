638c9713bf7d5e2ecba6df6d8961681eee961bc1
Sujwal <dsujwal24@gmail.com>
2026-02-04 12:21:06 +0545

    minimum length of string

--- twoPointers.js
+++ twoPointers.js
@@ -45,4 +45,119 @@ var longestSubarrayBetter = function (aee, k) {
   }
   return maxLen;
 }; // tc = O(n + n) = O(2n). // sc = O(1)
-console.log(longestSubarrayBetter(aee, k));
+// console.log(longestSubarrayBetter(aee, k));
+
+//
+
+//
+let s = "abca";
+// let s = "acbca";
+
+var checkPalindrome = function (s, left, right) {
+  let i = left;
+  let j = right;
+  let isPalindrome = true;
+
+  while (i < j) {
+    if (s.charAt(i) != s.charAt(j)) {
+      isPalindrome = false;
+      break;
+    }
+    i++;
+    j--;
+  }
+
+  return isPalindrome;
+};
+
+var validPalindrome = function (s) {
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    if (s.charAt(left) != s.charAt(right)) {
+      return (
+        checkPalindrome(s, left + 1, right) ||
+        checkPalindrome(s, left, right - 1)
+      );
+    }
+    left++;
+    right--;
+  }
+  return true;
+};
+// console.log(validPalindrome(s));
+
+//
+
+//
+let nums = [-4, -1, 0, 3, 10];
+var sortedSquares = function (nums) {
+  let n = nums.length;
+  let left = 0;
+  let right = n - 1;
+  let result = new Array(n);
+  let pos = n - 1;
+
+  while (left <= right) {
+    if (Math.abs(nums[left]) > Math.abs(nums[right])) {
+      result[pos] = nums[left] * nums[left];
+      left++;
+    } else {
+      result[pos] = nums[right] * nums[right];
+      right--;
+    }
+    pos--;
+  }
+  return result;
+};
+// console.log(sortedSquares(nums));
+
+//
+
+//...........
+let numbers = [2, 7, 11, 15];
+let target = 9;
+var twoSum = function (numbers, target) {
+  let left = 0;
+  let right = numbers.length - 1;
+
+  while (left < right) {
+    let sum = numbers[left] + numbers[right];
+
+    if (sum === target) {
+      return [left + 1, right + 1];
+    } else if (sum > target) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+};
+// console.log(twoSum(numbers, target));
+
+//
+
+//
+let str1 = "cabaabac";
+var minimumLength = function (s) {
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    if (s[left] != s[right]) {
+      break;
+    } else {
+      let char = s[left];
+
+      while (left <= right && s[left] == char) {
+        left++;
+      }
+      while (left <= right && s[right] == char) {
+        right--;
+      }
+    }
+  }
+  return right - left + 1;
+};
+console.log(minimumLength(str1));
