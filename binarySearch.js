d39a6936613d546c0f8bcfcdaffa408301d87b3b
Sujwal <dsujwal24@gmail.com>
2026-02-12 12:34:05 +0545

    Perfect Square and Arrange Coins done

---
+++ binarySearch.js
@@ -0,0 +1,280 @@
+// Rotated Array
+var search = function (nums, target) {
+  let low = 0;
+  let high = nums.length - 1;
+
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+
+    if (target === nums[mid]) return mid;
+    // left sorted
+    if (nums[low] <= nums[mid]) {
+      if (nums[low] <= target && target <= nums[mid]) {
+        high = mid - 1;
+      } else low = mid + 1;
+    } else {
+      if (nums[high] >= target && target >= nums[mid]) {
+        low = mid + 1;
+      } else high = mid - 1;
+    }
+  }
+  return -1;
+};
+
+// Missing Number - XOR Method
+var missingNumber = function (nums) {
+  const n = nums.length;
+  let xor = 0;
+
+  // XOR all numbers from 0 to n
+  for (let i = 0; i <= n; i++) {
+    xor ^= i;
+  }
+
+  // XOR all numbers in the array
+  for (let num of nums) {
+    xor ^= num;
+  }
+  return xor;
+};
+
+// Square Root
+let mySqrt = function (x) {
+  let low = 0;
+  let high = Math.floor(x / 2);
+  let ans = 0;
+
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+
+    if (mid <= x / mid) {
+      ans = mid;
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+
+  return ans;
+};
+
+// console.log(mySqrt(8));
+
+//
+
+// Intersection of two arrays using Sets
+var intersection = function (nums1, nums2) {
+  let set = new Set(nums1);
+  let result = new Set();
+
+  for (let num of nums2) {
+    if (set.has(num)) {
+      result.add(num);
+    }
+  }
+  return [...result];
+};
+// console.log(intersection([4, 9, 5], [9, 4, 9, 8, 4]));
+
+// Using Binary Search
+var intersectionUsingBS = function (nums1, nums2) {
+  let sortedArray =
+    nums1.length > nums2.length
+      ? nums1.sort((a, b) => a - b)
+      : nums2.sort((a, b) => a - b);
+
+  let iterateArray = nums1.length > nums2.length ? nums2 : nums1;
+
+  let result = new Set();
+  for (let i = 0; i < iterateArray.length; i++) {
+    let target = iterateArray[i];
+    let low = 0;
+    let high = sortedArray.length - 1;
+
+    while (low <= high) {
+      let mid = Math.floor((low + high) / 2);
+      if (target === sortedArray[mid]) {
+        result.add(target);
+        break;
+      } else if (target > sortedArray[mid]) {
+        low = mid + 1;
+      } else {
+        high = mid - 1;
+      }
+    }
+  }
+  return [...result];
+};
+// console.log(intersectionUsingBS([1, 2, 2, 1], [2, 2]));
+
+//
+
+//
+var intersect = function (nums1, nums2) {
+  let freqMap = new Map();
+  let result = [];
+
+  let [smaller, larger] =
+    nums1.length < nums2.length ? [nums1, nums2] : [nums2, nums1];
+
+  for (let num of smaller) {
+    if (freqMap.has(num)) {
+      freqMap.set(num, freqMap.get(num) + 1);
+    } else {
+      freqMap.set(num, 1);
+    }
+  }
+
+  for (let num of larger) {
+    if (freqMap.has(num) && freqMap.get(num) > 0) {
+      result.push(num);
+      freqMap.set(num, freqMap.get(num) - 1);
+    }
+  }
+  return result;
+};
+// console.log(intersect([1, 2, 2, 1], [2, 2]));
+
+//
+
+//
+var intersectUsing2P = function (nums1, nums2) {
+  nums1 = nums1.sort((a, b) => a - b);
+  nums2 = nums2.sort((a, b) => a - b);
+
+  let i = 0;
+  let j = 0;
+  let result = [];
+
+  while (i < nums1.length && j < nums2.length) {
+    if (nums1[i] === nums2[j]) {
+      result.push(nums1[i]);
+      i++;
+      j++;
+    } else if (nums1[i] < nums2[j]) {
+      i++;
+    } else j++;
+  }
+  return result;
+};
+
+// console.log(intersectUsing2P([1, 2, 2, 1], [2, 2]));
+
+//
+
+//
+let letters = ["c", "f", "j"];
+let target = "a";
+var nextGreatestLetter = function (letters, target) {
+  let ans = letters[0];
+  let left = 0;
+  let right = letters.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (letters[mid] > target) {
+      ans = letters[mid];
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return ans;
+};
+// console.log(nextGreatestLetter(letters, target));
+
+//
+
+//
+let aliceSizes = [1, 2, 5, 9];
+let bobSizes = [3, 4, 6, 8];
+var fairCandySwap = function (aliceSizes, bobSizes) {
+  let sumA = aliceSizes.reduce((acc, val) => acc + val, 0);
+  let sumB = bobSizes.reduce((acc, val) => acc + val, 0);
+  let result = new Array(2);
+  let diff = (sumA - sumB) / 2;
+
+  if ((sumA - sumB) % 2 !== 0) return result;
+
+  for (let i = 0; i < aliceSizes.length; i++) {
+    for (let j = 0; j < bobSizes.length; j++) {
+      // if (
+      //   sumA - aliceSizes[i] + bobSizes[j] ==
+      //   sumB + aliceSizes[i] - bobSizes[j]
+      // )
+      if (aliceSizes[i] === bobSizes[j] + diff) {
+        result[0] = aliceSizes[i];
+        result[1] = bobSizes[j];
+        return result;
+      }
+    }
+  }
+  return result;
+};
+
+// console.log(fairCandySwap(aliceSizes, bobSizes));
+
+//
+var fairCandySwapUsingSets = function (aliceSizes, bobSizes) {
+  let sumA = aliceSizes.reduce((acc, val) => acc + val, 0);
+  let sumB = bobSizes.reduce((acc, val) => acc + val, 0);
+  let diff = (sumA - sumB) / 2;
+
+  if ((sumA - sumB) % 2 !== 0) return [];
+
+  let aliceSet = new Set(aliceSizes);
+
+  // Loop through bob once
+  for (let b of bobSizes) {
+    let needed = b + diff;
+    // Lookup
+    if (aliceSet.has(needed)) {
+      return [needed, b];
+    }
+  }
+  return [];
+};
+
+// console.log(fairCandySwapUsingSets(aliceSizes, bobSizes));
+
+//
+
+//
+var isPerfectSquare = function (num) {
+  if (num < 2) return true;
+
+  let left = 1;
+  // let right = num;
+  let right = Math.floor(num / 2);
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    let square = mid * mid;
+
+    if (square === num) {
+      return true;
+    } else if (square > num) {
+      right = mid - 1;
+    } else left = mid + 1;
+  }
+  return false;
+};
+
+// console.log(isPerfectSquare(25));
+
+//
+
+//
+var arrangeCoins = function (n) {
+  let coinsLeft = n;
+  let row = 0;
+
+  while (coinsLeft >= row) {
+    coinsLeft -= row;
+    row++;
+  }
+
+  return row - 1;
+};
+console.log(arrangeCoins(5));
