4586e997c1f8c43626f3bc1751f8cd69255f2fbf
Sujwal <dsujwal24@gmail.com>
2026-02-17 12:29:35 +0545

    Tree day 1

---
+++ Tree.js
@@ -0,0 +1,13 @@
+//  Most asked questions in interview
+// Invert Binary Tree
+// Maximum Depth
+// Validate BST
+// Level Order Traversal
+// Lowest Common Ancestor
+// Diameter of Binary Tree
+// Serialize & Deserialize
+// Kth smallest in BST
+// Path Sum
+// Balanced Binary Tree
+
+//
