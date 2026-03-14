ab6e32323d7dd9a668035c2befc8690906538880
Sujwal <dsujwal24@gmail.com>
2026-03-14 15:00:43 +0545

    Day 3 of DP

--- DP/bottom-up.js
+++ DP/bottom-up.js
@@ -24,3 +24,25 @@ var fib0Space = function (n) {
   return b;
 };
 // console.log(fib0Space(6));
+
+// Knapsack problem
+var knabsack = function (wt, val, W) {
+  let n = wt.length;
+
+  let dp = new Array(n + 1).fill().map(() => new Array(W + 1).fill(0));
+
+  for (let i = 1; i < n + 1; i++) {
+    for (let w = 1; w < W + 1; w++) {
+      if (wt[i - 1] <= w) {
+        dp[i][w] = Math.max(
+          val[i - 1] + dp[i - 1][w - wt[i - 1]],
+          dp[i - 1][w],
+        );
+      } else {
+        dp[i][w] = dp[i - 1][w];
+      }
+    }
+  }
+  return dp[n][W];
+};
+console.log(knabsack([1, 3, 4, 5], [1, 4, 5, 7], 7));
--- DP/top-down.js
+++ DP/top-down.js
@@ -1,31 +1,57 @@
-let n = 100;
+var fibonacci = function (n) {
+  let dp = new Array(n + 1).fill(-1);
+  var fibo = function (n) {
+    if (n <= 1) return n;
 
-let dp = new Array(n + 1).fill(-1);
-var fibo = function (n) {
-  if (n <= 1) return n;
-
-  if (dp[n] !== -1) {
+    if (dp[n] !== -1) {
+      return dp[n];
+    }
+    dp[n] = fibo(n - 1) + fibo(n - 2);
     return dp[n];
-  }
-
-  dp[n] = fibo(n - 1) + fibo(n - 2);
-  return dp[n];
+  };
+  return fibo(n);
 };
-
-// console.log(fibo(n));
+// console.log(fibonacci(6));
 
 //
-// 0/1 Knapsack Problem
-var knabsack = function (wt, val, w, n = wt.length) {
-  if (n === 0 || w === 0) return 0;
+// 0/1 Knapsack Problem // Recursive
+// var knabsack = function (wt, val, w, n = wt.length) {
+//   if (n === 0 || w === 0) return 0;
 
-  if (wt[n - 1] <= w) {
-    return Math.max(
-      val[n - 1] + knabsack(wt, val, w - wt[n - 1], n - 1),
-      knabsack(wt, val, w, n - 1),
-    );
-  } else if (wt[n - 1] > w) {
-    return knabsack(wt, val, w, n - 1);
+//   if (wt[n - 1] <= w) {
+//     return Math.max(
+//       val[n - 1] + knabsack(wt, val, w - wt[n - 1], n - 1),
+//       knabsack(wt, val, w, n - 1),
+//     );
+//   }
+//   return knabsack(wt, val, w, n - 1);
+// };
+// console.log("Knabsack:", knabsack([1, 3, 4, 5], [1, 4, 5, 7], 7));
+
+//
+
+// Memoization
+var memoization = function (wt, val, w, n = wt.length) {
+  let dp = new Array(n + 1).fill().map(() => new Array(w + 1).fill(-1));
+
+  function knabsack(wt, val, w, n) {
+    if (n === 0 || w === 0) return 0;
+
+    if (dp[n][w] !== -1) {
+      return dp[n][w];
+    }
+
+    if (wt[n - 1] <= w) {
+      dp[n][w] = Math.max(
+        val[n - 1] + knabsack(wt, val, w - wt[n - 1], n - 1),
+        knabsack(wt, val, w, n - 1),
+      );
+    } else {
+      dp[n][w] = knabsack(wt, val, w, n - 1);
+    }
+    return dp[n][w];
   }
+  knabsack(wt, val, w, n);
+  return dp[n][w];
 };
-console.log("Knabsack:", knabsack([1, 3, 4, 5], [1, 4, 5, 7], 7));
+console.log("Knabsack:", memoization([1, 3, 4, 5], [1, 4, 5, 7], 7));
